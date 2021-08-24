11a0e53a328b256a236195b626a8b4406f5215bc
Rommel Blas <rc.blas@hotmail.com>
2021-08-25 02:13:39 +0800

    Initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,25 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      animation: false,
+      responsive: true,
+      renderOnClick: false
+    })
   ],
   declarations: [HomePage]
 })
